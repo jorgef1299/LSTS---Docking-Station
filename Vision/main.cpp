74f444e2a8003baf6d46df51456ddf1c445e53df
avrocha <avrocha99@gmail.com>
2020-09-21 14:05:44 +0100

    opening and closing filtering added

--- Vision/main.cpp
+++ Vision/main.cpp
@@ -4,54 +4,21 @@
 #include <iostream>
 
 using namespace cv;
-using namespace std;
-const int max_value_H = 360/2;
-const int max_value = 255;
-const String window_detection_name = "Object Detection";
-int low_H = 5, low_S = 0, low_V = 0;
-int high_H = 170, high_S = max_value, high_V = max_value;
-/*static void on_low_H_thresh_trackbar(int, void *)
-{
-    low_H = min(high_H-1, low_H);
-    setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-    high_H = max(high_H, low_H+1);
-    setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-    low_S = min(high_S-1, low_S);
-    setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-    high_S = max(high_S, low_S+1);
-    setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-    low_V = min(high_V-1, low_V);
-    setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-    high_V = max(high_V, low_V+1);
-    setTrackbarPos("High V", window_detection_name, high_V);
-}*/
+
 int main(int argc, char* argv[])
 {
+    int low_H = 10, low_S = 0, low_V = 0;
+    int high_H = 170, high_S = 255, high_V = 255;
+    const String window_detection_name = "Object Detection";
+
     VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
     namedWindow(window_detection_name);
-    // Trackbars to set thresholds for HSV values
-    //createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-    //createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-    //createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
-    //createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
-    //createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
-    //createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
+
     Mat frame, frame_HSV, frame_threshold, edges;
+
+    Mat opening_kernel = getStructuringElement( MORPH_RECT , Size(10, 10));
+    Mat closing_kernel = getStructuringElement( MORPH_RECT , Size(10, 10));
+
     while (true) {
         cap >> frame;
         if(frame.empty())
@@ -60,18 +27,27 @@ int main(int argc, char* argv[])
         }
         // Convert from BGR to HSV colorspace
         cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
+        
         // Detect the object based on HSV Range Values
         inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
-        // Show the frames
         frame_threshold=~frame_threshold;
-        medianBlur(frame_threshold, frame_threshold, 5);
-        Canny(frame_threshold,edges,50,200);
+
+        // Show the frames / Smoothening & Filtering
+        // Median Smothenigng
+        medianBlur(frame_threshold, frame_threshold, 3);
+        
+        // Opening and Closing filtering - closing maybe not helping
+        morphologyEx(frame_threshold, frame_threshold, MORPH_OPEN, opening_kernel);
+        // morphologyEx(frame_threshold, frame_threshold, MORPH_CLOSE, closing_kernel);
+        
+        // Others 
+        // Canny(frame_threshold,edges,50,200);
+
         imshow(window_detection_name, frame_threshold);
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27)
-        {
+        
+        
+        if ((char)waitKey(30) == 27)
             break;
-        }
     }
     return 0;
 }
