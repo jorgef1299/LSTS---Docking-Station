cf002e8f43c8f24927ba4ad9eed925ea418bab33
avrocha <avrocha99@gmail.com>
2020-10-11 18:11:00 +0100

    update organization

---
+++ Vision/build/CMakeFiles/circle_detection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circle_detection.dir/circle_detection.cpp.o"
+  "circle_detection"
+  "circle_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circle_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Vision/build/CMakeFiles/template_matching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_matching.dir/template_matching.cpp.o"
+  "template_matching"
+  "template_matching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
