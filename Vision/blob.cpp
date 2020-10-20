e4bc6d10190bc1629ce3bd6f44d9862193a0229f
avrocha <avrocha99@gmail.com>
2020-10-20 17:47:16 +0100

    camera calibration added

--- Vision/blob.cpp
+++ Vision/blob.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
+
 #include <opencv2/features2d.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
@@ -20,6 +22,26 @@ void use_only_ROI(Mat &frame_src){
   frame_src = frame_src(region_of_interest);
 }
 
+void read_calib_data(Mat &camera_matrix, Mat &dist_coefs){
+
+  double cam_matrix_data[3][3] = {
+    {9.333920501212203362e+02,0.000000000000000000e+00,6.415241498879005348e+02},
+    {0.000000000000000000e+00,9.342681978362431892e+02,3.549819986007769899e+02},
+    {0.000000000000000000e+00,0.000000000000000000e+00,1.000000000000000000e+00}
+  };
+
+  double dist_matrix_data[5] = {
+    4.523574472552951281e-02,
+    1.166409811351493686e-01,
+    3.820652804937282529e-03,
+    -1.049689355275789000e-02,
+    -6.592915665592768981e-01
+  };
+
+  camera_matrix = Mat(3, 3, CV_32F, cam_matrix_data);
+  dist_coefs = Mat(1, 5, CV_32F, dist_matrix_data);
+}
+
 int main(int argc, char *argv[]) {
 
   bool first_loop = true;
@@ -29,7 +51,10 @@ int main(int argc, char *argv[]) {
 
   Mat cap_frame, im_with_keypoints, blob_image;
   Mat kernel = getStructuringElement(MORPH_RECT, Size(10, 10));
- 
+
+  Mat camera_matrix, dist_coefs, calib_frame;
+  read_calib_data(camera_matrix, dist_coefs);
+
   // Setup SimpleBlobDetector parameters.
   SimpleBlobDetector::Params params;
 
@@ -52,29 +77,26 @@ int main(int argc, char *argv[]) {
   params.filterByInertia = true;
   params.minInertiaRatio = 0.01;
 
-  // Detect blobs.
   vector<KeyPoint> keypoints;
   Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
 
   while (true) {
     cap.read(cap_frame);
+
     use_only_ROI(cap_frame);
     
-    // Convert from RGB to HSV colorspace
     cvtColor(cap_frame, cap_frame, COLOR_BGR2HSV);
 
     // Detect the object based on HSV Range Values (returns monochromatic image)
     inRange(cap_frame, Scalar(10, 0, 0),
             Scalar(170, 255, 255), cap_frame);
 
-    GaussianBlur(cap_frame, cap_frame, Size(7, 7), 1);
+    GaussianBlur(cap_frame, cap_frame, Size(9, 9), 2);
     morphologyEx(cap_frame, cap_frame, MORPH_CLOSE, kernel); 
 
     detector->detect(cap_frame, keypoints);
 
     // Draw detected blobs as red circles.
-    // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle
-    // corresponds to the size of blob
     drawKeypoints(cap_frame, keypoints, im_with_keypoints, Scalar(0, 0, 255),
                   DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
@@ -86,7 +108,12 @@ int main(int argc, char *argv[]) {
     }
     
     imshow("Blob Detection", im_with_keypoints);
-    // imshow("Blob Detection", cap_frame);
+
+    // UNDISTORTION
+    // undistort(cap_frame, calib_frame, camera_matrix, dist_coefs);
+
+    // imshow("Original Frame", cap_frame);
+    // imshow("Calib Frame", cap_frame);
 
     if ((char)waitKey(30) == 27)
       break;
